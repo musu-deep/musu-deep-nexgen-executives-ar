230cebce354e09643cb2d63006fc96a52b481c3e
LAIA <louiabdalla1@gmail.com>
2026-07-26 22:46:03 +0300

    Route non-executive users to personal workspaces

--- frontend/src/pages/RoleAwareDashboardPage.jsx
+++ frontend/src/pages/RoleAwareDashboardPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import HRExecutiveDashboard, { isHumanResourcesUser } from "../components/HRExecutiveDashboard";
+import PersonalExecutiveDashboard from "../components/PersonalExecutiveDashboard";
 import ExecutiveDashboardPage from "./ExecutiveDashboardPage";
+import { isFullAccessUser } from "../lib/accessPolicy";
 
 export default function RoleAwareDashboardPage() {
   const { user } = useAuth();
 
-  if (isHumanResourcesUser(user)) {
-    return <HRExecutiveDashboard user={user} />;
-  }
-
-  return <ExecutiveDashboardPage />;
+  if (isFullAccessUser(user)) return <ExecutiveDashboardPage />;
+  if (isHumanResourcesUser(user)) return <HRExecutiveDashboard user={user} />;
+  return <PersonalExecutiveDashboard user={user} />;
 }
